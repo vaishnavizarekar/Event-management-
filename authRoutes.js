abfd8859bbdf92bb3a49b9892f415474f64ef59c
vaishnavizarekar <147190304+vaishnavizarekar@users.noreply.github.com>
2024-03-19 23:49:10 +0530

    authRoutes.js

---
+++ authRoutes.js
@@ -0,0 +1,8 @@
+// authRoutes.js
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+
+// Define routes for user registration, login, etc.
+
+module.exports = router;
